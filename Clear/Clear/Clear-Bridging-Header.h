f2161bba1da078c705a3c78024271ad8235e9068
Exr0n <spotyie@gmail.com>
2020-04-10 20:25:27 -0700

    base ARKit project

---
+++ Clear/Clear/Clear-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Clear-Bridging-Header.h
+//  Clear
+//
+//  Created by Albert on 4/10/20.
+//  Copyright © 2020 hackeer. All rights reserved.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ShaderTypes.h"
