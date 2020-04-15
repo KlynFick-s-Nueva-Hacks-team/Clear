2fcdfa97251f9723de8dfb3e640f10d2a47e911f
TheEnquirer <huxmarv@nuevaschool.org>
2020-04-14 19:35:33 -0700

    Added texturing + reformated torus + added people oclustion.

--- Cleer/Cleer/ViewController.swift
+++ Cleer/Cleer/ViewController.swift
@@ -11,11 +11,17 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
+   
+    
+    
+    
+    
 
     @IBOutlet var sceneView: ARSCNView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         
         // Set the view's delegate
         sceneView.delegate = self
@@ -26,17 +32,52 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Create a new scene
         let scene = SCNScene()
         
-        let opacity: CGFloat = 0.5
         
         let sphere = SCNSphere(radius: 0.1)
+        let torus = SCNTorus(ringRadius: 1.0, pipeRadius: 0.05)
+        let box = SCNBox(width: 0.2, height: 0.2, length: 0.2, chamferRadius: 0)
+    
         
+        
+        let opacity: CGFloat = 0.7
+        
+        let material = SCNMaterial()
+        
+
+        
+//        material.diffuse.contents = UIImage(named: "grimyMetal.png")
+        material.diffuse.contents = UIImage(named: "grimyMetal.png")
+
+//        material.diffuse.contents = UIColor.red
+        
+        torus.materials = [material]
+        sphere.materials = [material]
+//        box.materials = [material]
+        
+      
+        let torusNode = SCNNode(geometry: torus)
         let sphereNode = SCNNode(geometry: sphere)
+        let boxNode = SCNNode(geometry: box)
+       
+      
         
-        
+        torusNode.position = SCNVector3(0, 0, 0)
         sphereNode.position = SCNVector3(0, 0, -0.5)
-        sphereNode.opacity = opacity
+        boxNode.position = SCNVector3(0,0,-0.5)
+        
+//        sphereNode.opacity = opacity
+//        torusNode.opacity = opacity
+        
         
         scene.rootNode.addChildNode(sphereNode)
+        scene.rootNode.addChildNode(torusNode)
+//        scene.rootNode.addChildNode(boxNode)
+        
+        
+       
+        
+        
+        
         
         
         
@@ -49,6 +90,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        
+        configuration.frameSemantics.insert(.personSegmentationWithDepth)
+        sceneView.session.run(configuration)
 
         // Run the view's session
         sceneView.session.run(configuration)
