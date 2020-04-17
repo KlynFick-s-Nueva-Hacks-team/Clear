da319bc75f1afa6ab22c078322af7bfdb6ac9ea0
FlyN Nick <nickassader@gmail.com>
2020-04-16 17:16:26 -0700

    The map has been worked on

---
+++ Cleer/Cleer/MapVC.swift
@@ -0,0 +1,156 @@
+//
+//  MapVC.swift
+//  Cleer
+//
+//  Created by Nicholas Assaderaghi on 4/16/20.
+//  Copyright © 2020 zachary. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+import MapKit
+import CoreLocation
+import CoreTelephony
+import SystemConfiguration
+
+class MapVC: UIViewController
+{
+    @IBOutlet weak var map: MKMapView!
+    
+    private let locationManager = CLLocationManager()
+    private let regionInMeters: Double = 1000
+    private let uid: String = UUID().uuidString
+    private let reachability = SCNetworkReachabilityCreateWithName(nil, "google.com")
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        checkLocationServices()
+        _ = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounting), userInfo: nil, repeats: true)
+    }
+    @objc func updateCounting()
+    {
+        checkReachable()
+        let db = Firestore.firestore()
+        /*db.collection("Locations")
+            .getDocuments() { (querySnapshot, err) in
+                if let err = err
+                {
+                    print("Error getting documents: \(err)")
+                    let alert = UIAlertController(title: "Something Went Wrong", message: "An error occured while querying to the database.", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: nil))
+                }
+                else
+                {
+                    for document in querySnapshot!.documents
+                    {
+                        let longitude = document.data()["Longitude"] as! [Int]
+                        let latitude = document.data()["Latitude"] as! [Int]
+                        let probabilities = document.data()["Probabilities"] as! [Int]
+                        let userID = document.data()["UserID"] as! String
+                    }
+                }
+        }*/
+    }
+    private func plotLocations(locations: [[String:Any]])
+    {
+        for location in locations
+        {
+            let annotation = MKPointAnnotation()
+            annotation.title = location["title"] as? String
+            annotation.coordinate = CLLocationCoordinate2D(latitude: location["latitude"] as! CLLocationDegrees, longitude: location["longitude"] as! CLLocationDegrees)
+            map.addAnnotation(annotation)
+        }
+    }
+    private func setupLocationManager()
+    {
+        locationManager.delegate  = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    private func checkLocationServices()
+    {
+        if CLLocationManager.locationServicesEnabled()
+        {
+            setupLocationManager()
+            checkLocationAuthorization()
+        }
+        else
+        {
+            let alert = UIAlertController(title: "Location inacessible", message: "Location services are needed for \"Stear Cleer\"", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    private func centerViewOnUserLocation()
+    {
+        if let location = locationManager.location?.coordinate
+        {
+            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+            map.setRegion(region, animated: true)
+            map.isZoomEnabled = true
+        }
+    }
+    private func checkLocationAuthorization()
+    {
+        switch CLLocationManager.authorizationStatus()
+        {
+            case .authorizedWhenInUse:
+                map.showsUserLocation = true
+                centerViewOnUserLocation()
+                locationManager.startUpdatingLocation()
+                break
+            case .denied:
+                let alert = UIAlertController(title: "Location inacessible", message: "Location services are needed for \"Stear Cleer\"", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            case .notDetermined:
+                locationManager.requestWhenInUseAuthorization()
+            case .restricted:
+                let alert = UIAlertController(title: "Location inacessible", message: "Parental controls is preventing location usage.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+                break
+            case .authorizedAlways:
+                break
+            @unknown default:
+                break
+            }
+    }
+    private func checkReachable()
+    {
+        var flags = SCNetworkReachabilityFlags()
+        SCNetworkReachabilityGetFlags(self.reachability!, &flags)
+        
+        if (!isNetworkReachable(with: flags))
+        {
+            let alert = UIAlertController(title: "Internet Connection Required", message: "You can turn on mobile data for this app in Settings.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    private func isNetworkReachable (with flags: SCNetworkReachabilityFlags) -> Bool
+    {
+        let isReachable = flags.contains(.reachable)
+        let needsConnection = flags.contains(.connectionRequired)
+        let canConnectAutomatically = flags.contains(.connectionOnDemand) || flags.contains(.connectionOnTraffic)
+        let canConnectWithoutUserInteraction = canConnectAutomatically && !flags.contains(.interventionRequired)
+        return isReachable && (!needsConnection || canConnectWithoutUserInteraction)
+    }
+}
+extension MapVC: CLLocationManagerDelegate
+{
+    // MARK: - when user's location changes. I'll probs use this to query to firebase
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+        guard let location = locations.last else { return }
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion.init(center: center, latitudinalMeters: regionInMeters, longitudinalMeters:
+            regionInMeters)
+        map.setRegion(region, animated: true)
+    }
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus)
+    {
+        checkLocationAuthorization()
+    }
+}
